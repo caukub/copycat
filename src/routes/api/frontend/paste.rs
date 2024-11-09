38810ed4136efbe5f6fc0139ac330490c377dc66
green-threads <jakub.trumpes@gmail.com>
2024-11-09 18:27:37 +0100

    escape no-log API response

--- src/routes/api/frontend/paste.rs
+++ src/routes/api/frontend/paste.rs
@@ -84,7 +84,7 @@ pub async fn get_paste(
                 .map_err(|_| ApiError::Client("Couldn't read file to String"))?;
 
         let response = NoLogResponse {
-            content,
+            content: html_escape::encode_text(&content).to_string(),
             paste_type,
         };
 
